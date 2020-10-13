c8b9cd0e8a36c8dff64e370e4f9f8ccd16ee33b4
宮田将義 <masayamatu@miyatasusumuginoMacBook-puro.local>
2020-10-13 21:47:08 +0900

    R021013 update

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView , DetailView,CreateView,UpdateView
+from django.views.generic import ListView , DetailView,CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 
 from .models import Post
 
@@ -23,4 +24,10 @@ class BlogUpdateView(UpdateView):
     template_name = "post_edit.html"
     fields = ['title','body']
 
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    fields = ['title','body']
+    success_url = reverse_lazy('home')
 # Create your views here.
